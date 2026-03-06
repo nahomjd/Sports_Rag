9c25871b255b9deec06a4134718763d925c26863
Jackson Nahom <jackson.nahom@gmail.com>
2026-03-06 13:23:01 -0600

    Update summary_AI.py

--- summary_AI.py
+++ summary_AI.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.document_loaders import BSHTMLLoader # BeautifulSoup HTML Loader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.chains.summarize import load_summarize_chain
